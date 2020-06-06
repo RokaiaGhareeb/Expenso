ded573e684d82d1977657ca66ef9158a148ae697
Rokaia Ghareeb <Rokaiaessam97@gmail.com>
2020-06-07 01:55:17 +0200

    add some responsivity to the apllication

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,18 @@ import 'package:expense_tracker/widgets/chart.dart';
 import 'package:expense_tracker/widgets/new_transactions.dart';
 import 'package:expense_tracker/widgets/transactions_list.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'module/transactions.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown,
+    DeviceOrientation.portraitUp]
+  );
+  runApp(MyApp());
+} 
 
 class MyApp extends StatelessWidget {
   @override
@@ -23,41 +31,41 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransactions = [];
-  
-  List<Transaction> get _recentTransactions{
-    return _userTransactions.where((tx){
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
       return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
     }).toList();
   }
-  
-  void _addNewTransaction(String newTitle, String newAmount, DateTime selectedDate) {
+
+  void _addNewTransaction(
+      String newTitle, String newAmount, DateTime selectedDate) {
     final newtx = Transaction(
         title: newTitle,
         amount: double.parse(newAmount),
         date: selectedDate,
         id: DateTime.now().toString());
-        setState(() {
-          _userTransactions.add(newtx);
-        });
-  }
-
-  void _startNewTransaction(BuildContext ctx){
-    showModalBottomSheet(
-      isScrollControlled: true,      
-      context: ctx, builder: (_){
-      return  NewTransactions(_addNewTransaction);
-      
+    setState(() {
+      _userTransactions.add(newtx);
     });
   }
 
-  void _deleteTransaction(String id){
+  void _startNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+        isScrollControlled: true,
+        context: ctx,
+        builder: (_) {
+          return NewTransactions(_addNewTransaction);
+        });
+  }
+
+  void _deleteTransaction(String id) {
     setState(() {
       _userTransactions.removeWhere((tx) => tx.id == id);
     });
@@ -65,27 +73,36 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final appBar = AppBar(
+      title: Text('Expenso'),
+      actions: <Widget>[
+        IconButton(
+          icon: Icon(Icons.add),
+          onPressed: () => _startNewTransaction(context),
+        ),
+      ],
+    );
     return Scaffold(
       resizeToAvoidBottomPadding: false,
-      appBar: AppBar(
-        title: Text('Expenso'),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.add),
-        onPressed:() => _startNewTransaction(context),
-          ),
-        ],
-      ),
+      appBar: appBar,
       body: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Chart(_recentTransactions),
-            TransactionsList(_userTransactions, _deleteTransaction),
+            Container(
+                height: (MediaQuery.of(context).size.height -
+                    appBar.preferredSize.height -
+                    MediaQuery.of(context).padding.top ) * 0.35,
+                child: Chart(_recentTransactions)),
+            Container(
+               height: (MediaQuery.of(context).size.height -
+                    appBar.preferredSize.height -
+                    MediaQuery.of(context).padding.top ) * 0.5,
+                child: TransactionsList(_userTransactions, _deleteTransaction)),
           ]),
       floatingActionButton: FloatingActionButton(
         elevation: 30,
         child: Icon(Icons.add),
-        onPressed:() => _startNewTransaction(context),
+        onPressed: () => _startNewTransaction(context),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
--- lib/widgets/chartBar.dart
+++ lib/widgets/chartBar.dart
@@ -7,49 +7,59 @@ class ChartBar extends StatelessWidget {
   ChartBar(this.label, this.spendingAmount, this.percentageOfSpendedAmount);
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(
-          height: 20,
-          child: FittedBox(
-                    child: Text(
-              '\$${spendingAmount.toStringAsFixed(0)}',
-              style: TextStyle(fontSize: 18),
+    return LayoutBuilder(
+      builder: (ctx, constrains) {
+        return Column(
+          children: <Widget>[
+            Container(
+              height: constrains.maxHeight * 0.15,
+              child: FittedBox(
+                child: Text(
+                  '\$${spendingAmount.toStringAsFixed(0)}',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
             ),
-          ),
-        ),
-        SizedBox(height: 4),
-        Container(
-          width: 20,
-          height: 80,
-          child: Stack(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey, width: 1),
-                  color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              FractionallySizedBox(
-                heightFactor: percentageOfSpendedAmount,
-                
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Theme.of(context).primaryColor,
-                    borderRadius: BorderRadius.circular(10),
+            SizedBox(
+              height: constrains.maxHeight * 0.05,
+            ),
+            Container(
+              width: 20,
+              height: constrains.maxHeight * 0.6,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.grey, width: 1),
+                      color: Color.fromRGBO(220, 220, 220, 1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ),
+                  FractionallySizedBox(
+                    heightFactor: percentageOfSpendedAmount,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-        SizedBox(height: 4),
-        Text(
-          label,
-          style: TextStyle(fontSize: 18),
-        ),
-      ],
+            ),
+            SizedBox(
+              height: constrains.maxHeight * 0.05,
+            ),
+            Container(
+              height: constrains.maxHeight * 0.15,
+              child: Text(
+                label,
+                style: TextStyle(fontSize: 18),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
